a2e8a44b2a525ffabc74a1ece2abb72c9659480a
Kathryn Kessler <kmf1871@gmail.com>
2023-02-06 22:53:47 -0500

    updating gitignore file

--- WeatherPy/Starter_Code/starter_code/api_keys.py
+++ WeatherPy/Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "a2354b8d9b3f3b1c83b4b5bd95caba6b51ecf8f9"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "6ac5c7fc6b7c4ee396228031f5ef8d28"
